34bff4fe5e8ef12101f1f581560cd67db0cd4815
FedeOcejo <fede.ocejo.suquet@gmail.com>
2021-11-09 11:02:51 +0100

    Complete Code

--- inical.py
+++ inical.py
@@ -1,1 +1,18 @@
-print ("hello")
+import random
+numero = random.randint(0,99)
+respuesta = int (input ('introduce un numero entre 0 y 99'))
+intentos= 0
+
+while respuesta != numero:
+    if respuesta < numero:
+            print ("Muy Pequeño")
+            intentos = intentos + 1
+            respuesta = int (input ('introduce un numero entre o y 99'))
+    if respuesta > numero:
+            print ("Muy Grande")
+            intentos = intentos + 1
+            respuesta = int (input ('introduce un numero entre o y 99'))
+    if respuesta == numero:
+            print ("Felicidades")
+            intentos = intentos + 1
+            print ("Lo has conseguido en" ,intentos, "intentos")
